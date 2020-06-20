d041add4e163fdc90255ceaf792b8cb07566e845
YOGESH NILE <yogeshnile786@gmail.com>
2020-06-20 10:17:06 +0530

    Initial commit

---
+++ Spam SMS Prediction.py
@@ -0,0 +1,87 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+nltk.download('stopwords')
+import re
+import sklearn
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+import pickle
+
+sms = pd.read_csv('Spam SMS Collection', sep='\t', names=['label','message'])
+sms.drop_duplicates(inplace=True)
+sms.reset_index(drop=True, inplace=True)
+
+corpus = []
+ps = PorterStemmer()
+
+for i in range(0,sms.shape[0]):
+    message = re.sub(pattern='[^a-zA-Z]', repl=' ', string=sms.message[i]) #Cleaning special character from the message
+    message = message.lower() #Converting the entire message into lower case
+    words = message.split() # Tokenizing the review by words
+    words = [word for word in words if word not in set(stopwords.words('english'))] #Removing the stop words
+    words = [ps.stem(word) for word in words] #Stemming the words
+    message = ' '.join(words) #Joining the stemmed words
+    corpus.append(message) #Building a corpus of messages
+
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=2500)
+X = cv.fit_transform(corpus).toarray()
+
+y = pd.get_dummies(sms['label'])
+y = y.iloc[:, 1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+
+classifier = MultinomialNB(alpha=0.1)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+acc_s = accuracy_score(y_test, y_pred)*100
+print("Accuracy Score {} %".format(round(acc_s,2)))
+
+def predict_spam(sample_message):
+    sample_message = re.sub(pattern='[^a-zA-Z]',repl=' ', string = sample_message)
+    sample_message = sample_message.lower()
+    sample_message_words = sample_message.split()
+    sample_message_words = [word for word in sample_message_words if not word in set(stopwords.words('english'))]
+    ps = PorterStemmer()
+    final_message = [ps.stem(word) for word in sample_message_words]
+    final_message = ' '.join(final_message)
+    temp = cv.transform([final_message]).toarray()
+    return classifier.predict(temp)
+
+result = ['Wait a minute, this is a SPAM!','Ohhh, this is a normal message.']
+
+msg = "Hi! You are pre-qulified for Premium SBI Credit Card. Also get Rs.500 worth Amazon Gift Card*, 10X Rewards Point* & more. Click "
+
+if predict_spam(msg):
+    print(result[0])
+else:
+    print(result[1])
+
+
+msg = "[Update] Congratulations Nile Yogesh, You account is activated for investment in Stocks. Click to invest now: "
+
+if predict_spam(msg):
+    print(result[0])
+else:
+    print(result[1])
+
+msg = "Your Stock broker FALANA BROKING LIMITED reported your fund balance Rs.1500.5 & securities balance 0.0 as on end of MAY-20 . Balances do not cover your bank, DP & PMS balance with broking entity. Check details at YOGESHNILE.WORK4U@GMAIL.COM. If email Id not correct, kindly update with your broker."
+
+if predict_spam(msg):
+    print(result[0])
+else:
+    print(result[1])
+
+# Save Model
+with open('Spam SMS Prodiction','wb') as f:
+	pickle.dump(classifier,f)
+
